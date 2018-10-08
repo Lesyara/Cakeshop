82eda639bd7d62ab9b6c61668d2763c9a1899dd1
Lesyara <lesya.arhipenko@gmail.com>
2018-10-08 20:06:15 +0300

    Обновление файла сборки + конфиги

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,9 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var del = require("del");
-var run = require("run-sequence");
 var server = require("browser-sync").create();
-/*var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");*/
-
+var uglify = require('gulp-uglify-es').default;
+var pump = require('pump');
 
 gulp.task("style", function () {
   return gulp.src("source/less/style.less")
@@ -64,14 +61,24 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", function (done) {
-  run(
+gulp.task("compress", function (cb) {
+  pump([
+    gulp.src("source/js/*.js"),
+    uglify(),
+    rename({ suffix: ".min" }),
+    gulp.dest("build/js")
+  ],
+    cb
+  );
+});
+
+gulp.task("build",
+  gulp.series(
     "clean",
     "copy",
     "style",
-    done
-  );
-});
+    "compress"
+  ));
 
 gulp.task("serve", function () {
   server.init({
@@ -82,6 +89,6 @@ gulp.task("serve", function () {
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html", ["copy"]).on("change", server.reload);
+  gulp.watch("source/less/**/*.less", gulp.series("style"));
+  gulp.watch("source/*.html", gulp.series("copy")).on("change", server.reload);
 });
